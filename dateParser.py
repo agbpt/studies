188b131e07405a90ee1cd5e2e5cc142304d97a67
Alexandre Breda <alexandrebreda@MBPdeAlexandre.lan>
2017-04-29 14:56:31 +0100

    Atualizando dateParser.py

--- dateParser.py
+++ dateParser.py
@@ -1,17 +1,2 @@
-from datetime import datetime
-
-now = datetime.now()
-
-mm = str(now.month)
-
-dd = str(now.day)
-
-yyyy = str(now.year)
-
-hour = str(now.hour)
-
-mi = str(now.minute)
-
-ss = str(now.second)
-
-print mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss
+import datetime
+print(datetime.__file__)
